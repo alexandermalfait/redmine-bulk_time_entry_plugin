0e8d61545a9d48b83121264f9db5afa51b50a900
alexandermalfait <alexandermalfait@gmail.com>
2009-02-11 08:46:06 +0100

    Fix problem with issue link when you select issue by inputting the number

--- assets/javascripts/timeentry.js
+++ assets/javascripts/timeentry.js
@@ -1,220 +1,225 @@
 var TimeEntry = Class.create({
-  initialize: function(container) {
-     this.container = container;
+   initialize: function(container) {
+      this.container = container;
 
-     this.observeElements()
+      this.observeElements()
 
-     this.setInitialValuesFromLastRecord()
+      this.setInitialValuesFromLastRecord()
 
-     //focus the project pulldown by default
-     this.getElement('select.project-select').focus()
+      //focus the project pulldown by default
+      this.getElement('select.project-select').focus()
 
-     this.startTimer()
-  } ,
+      this.startTimer()
+   } ,
 
-  observeElements: function() {
-     //select an issue by number
-     this.getElement("input.jump-to-issue").observe('keyup',function(event) {
-        if(event.target.value) {
-           this.selectIssue(event.target.value)
-        }
-     }.bind(this))
+   observeElements: function() {
+      //select an issue by number
+      this.getElement("input.jump-to-issue").observe('keyup',function(event) {
+         if(event.target.value) {
+            this.selectIssue(event.target.value)
+         }
+      }.bind(this))
 
-     //save the last selected project, update issues
-     this.getElement('select.project-select').observe('change',function(event) {
-        TimeEntry.lastValues.projectId = event.target.value
-        this.updateIssues()
-     }.bind(this))
+      //save the last selected project, update issues
+      this.getElement('select.project-select').observe('change',function(event) {
+         TimeEntry.lastValues.projectId = event.target.value
+         this.updateIssues()
+      }.bind(this))
 
-     //save the last filter checkbox values, update issues
-     this.getElement('input.only-my-issues-checkbox').observe('click',function(event) {
-        TimeEntry.lastValues.onlyMyIssues = event.target.checked
-        this.updateIssues()
-     }.bind(this))
+      //save the last filter checkbox values, update issues
+      this.getElement('input.only-my-issues-checkbox').observe('click',function(event) {
+         TimeEntry.lastValues.onlyMyIssues = event.target.checked
+         this.updateIssues()
+      }.bind(this))
 
-     this.getElement('input.no-closed-issues-checkbox').observe('click',function(event) {
-        TimeEntry.lastValues.noClosedIssues = event.target.checked
-        this.updateIssues()
-     }.bind(this))
+      this.getElement('input.no-closed-issues-checkbox').observe('click',function(event) {
+         TimeEntry.lastValues.noClosedIssues = event.target.checked
+         this.updateIssues()
+      }.bind(this))
 
-     //save the last date
-     this.getElement('input.spent_on').observe('change',function(event) {
-        TimeEntry.lastValues.lastSpentOnDate = event.target.value
-     })
+      //save the last date
+      this.getElement('input.spent_on').observe('change',function(event) {
+         TimeEntry.lastValues.lastSpentOnDate = event.target.value
+      })
 
-     //observe the cancel button
-     this.getElement('button.cancel-button').observe('click',function(event) {
-        event.stop()
-        this.cancel()
-     }.bind(this))
+      //observe the cancel button
+      this.getElement('button.cancel-button').observe('click',function(event) {
+         event.stop()
+         this.cancel()
+      }.bind(this))
 
-     //obverse the toggle timer button
-     this.getElement('button.toggle-timer-button').observe('click',function(event) {
+      //obverse the toggle timer button
+      this.getElement('button.toggle-timer-button').observe('click',function(event) {
          event.stop()
 
          this.toggleTimer()
-     }.bind(this))
+      }.bind(this))
 
-     //when the form is submitted, the HTML element will disappear, so remove this entry
-     this.container.up('form').observe('submit',function() {
+      //when the form is submitted, the HTML element will disappear, so remove this entry
+      this.container.up('form').observe('submit',function() {
          this.remove()
-     }.bind(this))
-  } ,
+      }.bind(this))
+   } ,
 
-  bindIssueSelectorToLink: function() {
-     var issueSelector = this.getElement('select.issue-select')
+   bindIssueSelectorToLink: function() {
+      var issueSelector = this.getElement('select.issue-select')
 
-     if(issueSelector) {
-        var issueLink = this.getElement('a.issue-link')
+      if(issueSelector) {
+         issueSelector.observe('change',function() {
+            this.setIssueLinkUrl();
+         }.bind(this))
+      }
+   } ,
 
-        issueSelector.observe('change',function() {
-           if(issueSelector.value > 0) {
-              issueLink.writeAttribute("href", "/issues/show/" + issueSelector.value)
-              issueLink.writeAttribute("target", "_blank")
-           }
-           else {
-              issueLink.writeAttribute("href", "")
-              issueLink.writeAttribute("target", "")
-           }
-        }.bind(this))
-     }
-  } ,
+   setIssueLinkUrl: function() {
+      var issueLink = this.getElement('a.issue-link')
+      var issueSelector = this.getElement('select.issue-select')
 
-  setInitialValuesFromLastRecord: function() {
-     if(TimeEntry.lastValues.projectId) {
-        this.getElement('select.project-select').value = TimeEntry.lastValues.projectId
-     }
+      if(issueSelector.value > 0) {
+         issueLink.writeAttribute("href", "/issues/show/" + issueSelector.value)
+         issueLink.writeAttribute("target", "_blank")
+      }
+      else {
+         issueLink.writeAttribute("href", "")
+         issueLink.writeAttribute("target", "")
+      }
+   } ,
 
-     this.getElement('input.only-my-issues-checkbox').checked = TimeEntry.lastValues.onlyMyIssues
-     this.getElement('input.no-closed-issues-checkbox').checked = TimeEntry.lastValues.noClosedIssues
-     this.getElement('input.spent_on').value = TimeEntry.lastValues.lastSpentOnDate
+   setInitialValuesFromLastRecord: function() {
+      if(TimeEntry.lastValues.projectId) {
+         this.getElement('select.project-select').value = TimeEntry.lastValues.projectId
+      }
 
-     this.updateIssues()
-  } ,
+      this.getElement('input.only-my-issues-checkbox').checked = TimeEntry.lastValues.onlyMyIssues
+      this.getElement('input.no-closed-issues-checkbox').checked = TimeEntry.lastValues.noClosedIssues
+      this.getElement('input.spent_on').value = TimeEntry.lastValues.lastSpentOnDate
 
-  updateIssues: function() {
-     var params = {
-        project_id: $F(this.getElement('select.project-select')),
-        entry_id: this.container.id
-     }
+      this.updateIssues()
+   } ,
 
-     if(this.getElement('input.only-my-issues-checkbox').checked) {
-        params.assigned_to_id = TimeEntry.defaultUserId
-     }
+   updateIssues: function() {
+      var params = {
+         project_id: $F(this.getElement('select.project-select')),
+         entry_id: this.container.id
+      }
 
-     if(this.getElement('input.no-closed-issues-checkbox').checked) {
-        params.only_open = true
-     }
+      if(this.getElement('input.only-my-issues-checkbox').checked) {
+         params.assigned_to_id = TimeEntry.defaultUserId
+      }
 
-     new Ajax.Request(TimeEntry.loadIssuesUrl, {
-        parameters: params,
-        onComplete: function() {
-           this.bindIssueSelectorToLink()
-        }.bind(this)
-     })
-  } ,
+      if(this.getElement('input.no-closed-issues-checkbox').checked) {
+         params.only_open = true
+      }
 
-  selectIssue: function(issueId) {
-     var selector = this.container.down('select.issue-select')
+      new Ajax.Request(TimeEntry.loadIssuesUrl, {
+         parameters: params,
+         onComplete: function() {
+            this.bindIssueSelectorToLink()
+         }.bind(this)
+      })
+   } ,
 
-     if(selector) {
-        selector.value = issueId;
-     }
-  } ,
+   selectIssue: function(issueId) {
+      var selector = this.container.down('select.issue-select')
 
-  toggleTimer: function() {
-     if(this.timer == null) {
+      if(selector) {
+         selector.value = issueId;
+
+         this.setIssueLinkUrl()
+      }
+   } ,
+
+   toggleTimer: function() {
+      if(this.timer == null) {
          this.startTimer()
-     }
-     else {
+      }
+      else {
          this.stopTimer()
-     }
-  } ,
+      }
+   } ,
 
-  startTimer: function() {
+   startTimer: function() {
       this.stopAllTimers()
 
       var hoursInput = this.getElement('input.hours-input')
 
-      hoursInput.setStyle({backgroundColor:"#acfbc1"})
-      hoursInput.setStyle({border:"1px solid green"})
+      hoursInput.setStyle({ backgroundColor:"#acfbc1", border:"1px solid green" })
 
       this.timedMinutes = this.convertDecimalHoursToMinutes(hoursInput.value)
 
       this.getElement('button.toggle-timer-button').innerHTML = TimeEntry.language.stop_timer
 
       this.timer = new PeriodicalExecuter(function() {
-        this.timedMinutes++;
-        this.updateHoursField()
+         this.timedMinutes++;
+         this.updateHoursField()
       }.bind(this), 60)
-  } ,
+   } ,
 
-  stopTimer: function() {
+   stopTimer: function() {
       if(this.timer != null) {
-          this.timer.stop()
-          this.timer = null
+         this.timer.stop()
+         this.timer = null
       }
 
       this.getElement('button.toggle-timer-button').innerHTML = TimeEntry.language.start_timer
 
-      this.getElement('input.hours-input').setStyle({backgroundColor:"#faadb6"})
-      this.getElement('input.hours-input').setStyle({border:"1px solid red"})
-  } ,
+      this.getElement('input.hours-input').setStyle({ backgroundColor:"#faadb6", border:"1px solid red" })
+   } ,
 
-  stopAllTimers: function() {
+   stopAllTimers: function() {
       TimeEntry.entries.each(function(entry) {
-          entry.stopTimer()
+         entry.stopTimer()
       })
-  } ,
+   } ,
 
-  updateHoursField: function() {
+   updateHoursField: function() {
       this.getElement('input.hours-input').value = this.convertMinutesToDecimalHours(this.timedMinutes)
-  } ,
+   } ,
 
-  convertMinutesToDecimalHours: function(minutes) {
+   convertMinutesToDecimalHours: function(minutes) {
       return Math.round((minutes / 60) * 100) / 100
-  } ,
+   } ,
 
-  convertDecimalHoursToMinutes: function(hours) {
-     if(!hours) {
+   convertDecimalHoursToMinutes: function(hours) {
+      if(!hours) {
          return 0;
-     }
+      }
 
-     return parseFloat(hours) * 60
-  } ,
+      return parseFloat(hours) * 60
+   } ,
 
-  save: function() {
-     this.getElement('button.save-button').click()
-  } ,
+   save: function() {
+      this.getElement('button.save-button').click()
+   } ,
 
-  cancel: function() {
-     if(confirm(TimeEntry.language.are_you_sure)) {
-        this.container.up('form').remove()
-        this.remove()
-     }
-  } ,
+   cancel: function() {
+      if(confirm(TimeEntry.language.are_you_sure)) {
+         this.container.up('form').remove()
+         this.remove()
+      }
+   } ,
 
-  remove: function() {
-     TimeEntry.entries = TimeEntry.entries.without(this)
+   remove: function() {
+      TimeEntry.entries = TimeEntry.entries.without(this)
 
-     if(this.timer != null) {
+      if(this.timer != null) {
          this.timer.stop()
          this.timer = null
-     }
-  } ,
+      }
+   } ,
 
-  getElement: function(cssClass) {
-     return this.container.down(cssClass)
-  }
+   getElement: function(cssClass) {
+      return this.container.down(cssClass)
+   }
 })
 
 //static TimeEntry values and methods:
 
 TimeEntry.lastValues = {
-  projectId: null,
-  onlyMyIssues: true,
-  noClosedIssues: true,
-  lastSpentOnDate: null //this one is set from ruby
+   projectId: null,
+   onlyMyIssues: true,
+   noClosedIssues: true,
+   lastSpentOnDate: null //this one is set from ruby
 }
 
 TimeEntry.language = { }
@@ -222,7 +227,7 @@ TimeEntry.language = { }
 TimeEntry.entries = []
 
 TimeEntry.saveAllEntries = function() {
-  this.entries.each(function(entry) {
+   this.entries.each(function(entry) {
       entry.save()
-  })
+   })
 }
